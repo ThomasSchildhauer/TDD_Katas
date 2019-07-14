f284a28b20f9b520b5be1e7b000f690f3a21ba38
Thomas Schildhauer <Thomas.Schildhauer@outlook.com>
2019-07-14 09:52:21 +0200

    refactored StringCalculatorTest

--- StringCalculatorTest/StringCalculatorTest.cs
+++ StringCalculatorTest/StringCalculatorTest.cs
@@ -49,47 +49,8 @@ namespace Kata_StringCalculatorTest
         }
 
         [TestMethod]
-        public void CutString_Should_Return_0_And_0()
-        {
-            // ARRANGE
-            string numbers = "0,0";
-            StringCalculator SUT = new StringCalculator();
-            List<int> expected = new List<int>
-            {
-                0,
-                0
-            };
-
-            // EXECUTION
-            var actual = SUT.CutString(numbers);
-
-            // ASSERTION 
-            expected[0].Should().Be(actual[0]);
-            expected[1].Should().Be(actual[1]);
-        }
-
-        [TestMethod]
-        public void CutString_Should_Return_0_And_1()
-        {
-            // ARRANGE
-            string numbers = "0,1";
-            StringCalculator SUT = new StringCalculator();
-            List<int> expected = new List<int>
-            {
-                0,
-                1
-            };
-
-            // EXECUTION
-            var actual = SUT.CutString(numbers);
-
-            // ASSERTION 
-            expected[0].Should().Be(actual[0]);
-            expected[1].Should().Be(actual[1]);
-        }
-
-        [TestMethod]
         [DataRow("0,0", 0, 0)]
+        [DataRow("0,1", 0, 1)]
         public void CutString_Should_Return_Two_Correct_Numbers(string numbers, int expected1, int expected2)
         {
             // ARRANGE
